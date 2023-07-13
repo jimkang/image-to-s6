a1493471198f058b3e4b08ef9d38f34efa069ce9
Jim Kang <jimkang@gmail.com>
2023-07-12 22:32:06 -0400

    Hide obscuring Google sign-in container so logging in works.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /* global process */
-
+require('dotenv').config();
 var playwright = require('playwright');
 
 const nsInMS = 1e9 / 1000;
@@ -9,30 +9,15 @@ const defaultBrowserType = 'firefox';
 async function uploadToS6({ browserType = defaultBrowserType }) {
   var browser = await playwright[browserType].launch({ headless: false });
   var page = await browser.newPage();
-  await page.goto('https://society6.com/');
-  var loginButton = await page.locator(
-    '[role="button"][aria-label="join or login"]'
-  );
+  await page.goto('https://society6.com/artist-studio');
+  var loginButton = await page.getByRole('button', {
+    name: 'Log in / Sign up',
+  });
   await loginButton.waitFor();
   console.log('loginButton count', await loginButton.count());
   await loginButton.click();
 
-  // Wait for the Google junk to appear.
-  // Why doesn't the `waitFor` below suffice? I don't know.
-  await stall(5);
-
-  var obscuringContainer = await page.locator('#credential_picker_container');
-  try {
-    await obscuringContainer.waitFor();
-    await obscuringContainer.evaluate((node) =>
-      node.parentNode.removeChild(node)
-    );
-  } catch (error) {
-    console.error(
-      'Error while waiting for sign in with Google. Maybe they got rid of it? Hooray.',
-      error
-    );
-  }
+  await getRidOfSignInWithGoogle();
 
   var overlay = await page.locator('#overlay');
   await overlay.waitFor();
@@ -44,17 +29,45 @@ async function uploadToS6({ browserType = defaultBrowserType }) {
 
   await emailBox.click();
   await emailBox.fill(process.env.email);
+
   var passwordBox = await page.getByPlaceholder('Password');
   await passwordBox.waitFor();
-  // Click here in case the "sign in with Google" dialog has taken focus.
   await passwordBox.click();
+
+  // await getRidOfSignInWithGoogle();
   passwordBox.fill(process.env.password);
+
+  // await getRidOfSignInWithGoogle();
+
   var loginSubmit = await page.locator('form[name="login"] > button');
   await loginSubmit.click();
   await page.screenshot({ path: 'screenshot.png', fullPage: true });
 
   await stall(5);
   browser.close();
+
+  async function getRidOfSignInWithGoogle() {
+    try {
+      var obscuringContainer = await page.locator(
+        '#credential_picker_container'
+      );
+      await obscuringContainer.waitFor({ timeout: 5000 });
+
+      if ((await obscuringContainer.count()) > 0) {
+        await obscuringContainer.evaluate((node) =>
+          // node.parentNode.removeChild(node)
+          node.setAttribute('style', 'z-index: -1')
+        );
+      } else {
+        console.log('No Sign In With Google popover found.');
+      }
+    } catch (error) {
+      console.error(
+        'Error while waiting for sign in with Google. Maybe they got rid of it? Hooray.',
+        error
+      );
+    }
+  }
 }
 
 async function stall(seconds) {
