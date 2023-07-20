51f4a75e82a7856310903be9f33ffeab5c09d4b9
Jim Kang <jimkang@gmail.com>
2023-07-19 21:25:13 -0400

    Refactor.

--- index.js
+++ index.js
@@ -2,13 +2,20 @@
 require('dotenv').config();
 var playwright = require('playwright');
 
-const nsInMS = 1e9 / 1000;
+// const nsInMS = 1e9 / 1000;
 
 const defaultBrowserType = 'firefox';
 
 async function uploadToS6({ browserType = defaultBrowserType }) {
   var browser = await playwright[browserType].launch({ headless: false });
   var page = await browser.newPage();
+  await getToArtistStudio({ page });
+
+  await stall(5);
+  browser.close();
+}
+
+async function getToArtistStudio({ page }) {
   await page.goto('https://society6.com/artist-studio');
   var loginButton = await page.getByRole('button', {
     name: 'Log in / Sign up',
@@ -17,7 +24,7 @@ async function uploadToS6({ browserType = defaultBrowserType }) {
   console.log('loginButton count', await loginButton.count());
   await loginButton.click();
 
-  await getRidOfSignInWithGoogle();
+  await getRidOfSignInWithGoogle({ page });
 
   var overlay = await page.locator('#overlay');
   await overlay.waitFor();
@@ -34,39 +41,34 @@ async function uploadToS6({ browserType = defaultBrowserType }) {
   await passwordBox.waitFor();
   await passwordBox.click();
 
-  // await getRidOfSignInWithGoogle();
+  await getRidOfSignInWithGoogle({ page });
   passwordBox.fill(process.env.password);
 
-  // await getRidOfSignInWithGoogle();
+  await getRidOfSignInWithGoogle({ page });
 
   var loginSubmit = await page.locator('form[name="login"] > button');
   await loginSubmit.click();
   await page.screenshot({ path: 'screenshot.png', fullPage: true });
+}
 
-  await stall(5);
-  browser.close();
+async function getRidOfSignInWithGoogle({ page }) {
+  try {
+    var obscuringContainer = await page.locator('#credential_picker_container');
+    await obscuringContainer.waitFor({ timeout: 5000 });
 
-  async function getRidOfSignInWithGoogle() {
-    try {
-      var obscuringContainer = await page.locator(
-        '#credential_picker_container'
+    if ((await obscuringContainer.count()) > 0) {
+      await obscuringContainer.evaluate((node) =>
+        // node.parentNode.removeChild(node)
+        node.setAttribute('style', 'z-index: -1')
       );
-      await obscuringContainer.waitFor({ timeout: 5000 });
-
-      if ((await obscuringContainer.count()) > 0) {
-        await obscuringContainer.evaluate((node) =>
-          // node.parentNode.removeChild(node)
-          node.setAttribute('style', 'z-index: -1')
-        );
-      } else {
-        console.log('No Sign In With Google popover found.');
-      }
-    } catch (error) {
-      console.error(
-        'Error while waiting for sign in with Google. Maybe they got rid of it? Hooray.',
-        error
-      );
+    } else {
+      console.log('No Sign In With Google popover found.');
     }
+  } catch (error) {
+    console.error(
+      'Error while waiting for sign in with Google. Maybe they got rid of it? Hooray.',
+      error
+    );
   }
 }
 
